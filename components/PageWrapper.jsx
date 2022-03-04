abf5970e6a6f3ac3c691f513fd7643c8efc5d719
alexqguo <alexqguo@gmail.com>
2022-03-04 13:18:19 -0500

    Add some pages and base layout

--- components/PageWrapper.jsx
+++ components/PageWrapper.jsx
@@ -1,9 +1,20 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import Nav from 'components/Nav';
 
-const PageWrapper = ({ children }) => (
-  <main class="">
-    {children}
-  </main>
+const PageWrapper = ({
+  children,
+}) => (
+  <>
+    <Nav />
+    <main className="py-4 px-6">
+      {children}
+    </main>
+  </>
 );
+
+PageWrapper.propTypes = {
+  children: PropTypes.node.isRequired,
+};
 
 export default PageWrapper;
