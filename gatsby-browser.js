1d4554b7a8f34c1bb2f479da274049f2e1a066f5
alexqguo <alexqguo@gmail.com>
2022-03-04 11:52:32 -0500

    Clean dir

--- gatsby-browser.js
+++
@@ -1,4 +0,0 @@
-import 'typeface-noto-serif-jp';
-import './src/styles/default.css';
-import './src/styles/index.css';
-import './src/styles/blog.css';
--- src/components/common/header.js
+++
@@ -1,6 +0,0 @@
-// Not sure why I'm keeping this here, will use later if I want to
-const Header = () => {
-  return '';
-};
-
-export default Header;
