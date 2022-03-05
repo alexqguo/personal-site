4d6caef0a05895d04010322d21ffdac91aa19e6d
Alex Guo <alexqguo@gmail.com>
2022-03-05 15:56:35 -0500

    Update cards

--- components/Card.jsx
+++ components/Card.jsx
@@ -4,24 +4,33 @@ import PropTypes from 'prop-types';
 const Card = ({
   href,
   title,
+  repoLink,
   description,
 }) => (
-  <a
-    href={href}
-    target="_blank"
-    className="inline-block p-2 w-full bg-white rounded-md border border-gray-200 shadow-md hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700"
-  >
-    <div className="px-6 py-4">
-      <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{title}</h5>
-      <div className="text-sm text-gray-700 dark:text-gray-400">{href}</div>
-      <p className="font-normal text-gray-700 dark:text-gray-400">{description}</p>
+  <div className="inline-block p-2 w-full">
+    <div className="bg-white rounded-md border border-gray-200 shadow-md hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+      <a
+        href={href}
+        target="_blank"
+        className=""
+      >
+        <div className="px-6 py-4">
+          <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{title}</h5>
+          <div className="text-sm text-gray-700 dark:text-gray-400">{href}</div>
+          <p className="font-normal text-gray-700 dark:text-gray-400">{description}</p>
+        </div>
+      </a>
     </div>
-  </a>
+    <span className="text-xs italic text-gray-700 dark:text-gray-400">
+      Repo: <a href={repoLink} target="_blank">{repoLink}</a>
+    </span>
+  </div>
 );
 
 Card.propTypes = {
   href: PropTypes.string.isRequired,
   title: PropTypes.string.isRequired,
+  repoLink: PropTypes.string.isRequired,
   description: PropTypes.string.isRequired,
 };
 
--- pages/other-projects.js
+++ pages/other-projects.js
@@ -7,23 +7,26 @@ export default () => (
   <PageWrapper>
     <PageHead title="Other Projects" />
 
-    <div className="grid grid-cols-2 gap-4">
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
       <Card
         title="Pizza Box Game"
         href="https://pizza.alexguo.co"
-        description="Online blah blah blah blah blah blah blah blah"
+        repoLink="https://github.com/alexqguo/pizza-box-game"
+        description="Online drinking game based off of a Reddit comment"
       />
 
       <Card
         title="Drinking Board Game"
         href="https://drink.alexguo.co"
+        repoLink="https://github.com/alexqguo/drinking-board-game"
         description="This is a generalized engine for playing drinking board games online"
       />
 
       <Card
         title="Sorting Algo Viz w/ JS Proxies"
-        href="https://js-ol3vti.stackblitz.io/"
-        description="Online blah blah blah blah blah blah blah blah"
+        href="https://js-ol3vti.stackblitz.io"
+        repoLink="https://github.com/alexqguo/proxy-example"
+        description="Sorting algorithm visualization+audio (inspired from that YouTube video) using JS Proxy objects"
       />
     </div>
   </PageWrapper>
